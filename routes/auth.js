f5aedef271c36b78a70ec9816ac42fa3ddaabe94
HugoDMiranda <hdmirandab@hotmail.com>
2023-08-02 01:04:53 -0500

    password endpoint added

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/password", password);
+router.put("/password", password);
 
 export default router;
